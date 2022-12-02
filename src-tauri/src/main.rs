820146401e69bb1e0a9ef41b76d5946745676f8a
Flysoft <1922656933@qq.com>
2022-12-02 10:42:48 +0800

    fix(platform):  cannot run on non-Windows platform
    Only run `window_shadows::set_shadow` on Windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,9 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let main_window = app.get_window("main").unwrap();
-            window_shadows::set_shadow(&main_window, true).expect("Unsupported platform!");
+            if cfg!(target_os = "windows") {
+                window_shadows::set_shadow(&main_window, true).expect("Unsupported platform!");
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
