ded7cf9bebf180d3735cbe29e50b3985bde26da5
Flysoft <1922656933@qq.com>
2022-12-03 11:23:01 +0800

    feat(components): add detail page

---
+++ src/tabs/page.tsx
@@ -0,0 +1,11 @@
+import { Cover } from "../media/cover";
+
+function Page() {
+    return (
+        <div className="page">
+            <div className="page_info">
+                <Cover type="artist"></Cover>
+            </div>
+        </div>
+    );
+}
